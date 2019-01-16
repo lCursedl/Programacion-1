317e1e838c84fc29141c88b87083cb463ae27511
llCursedll <idv18c.galvarez@uartesdigitales.edu.mx>
2019-01-15 19:37:08 -0600

    Agregados ejercicios de capítulo 2.

---
+++ Capítulo 2/Ejercicio_1-2/Ejercicio_1-2/Ejercicio_1-2.cpp
@@ -0,0 +1,22 @@
+// Ejercicio_1-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, programming!\nHere we go!\n";
+	cout << "Lista instrucciones\n";
+	cout << "1.-Recorre la silla hacia atras 30 cm y levantate.\n";
+	cout << "2.-Camina, alternando el movimiento de tus piernas para desplazarte por el corredor.\n";
+	cout << "3.-Encuentra la apertura que va hacia la derecha, gira 90 grados y camina hacia la escalera.\n";
+	cout << "4.-Al llegar a la escalera, apoya cualquier pie en el escalon y camina alternando el pie que se apoya.\n";
+	cout << "5.-Si la escalera no tiene giros:\n\tA: Sigue caminando hasta haber cruzado el ultimo escalon.\n\tB: En caso de tenerlos, gira tu cuerpo en la nueva dirección de los escalones y sigue caminando.\n";
+	cout << "6.-Al llegar al final de la escaalera gira tu cuerpo -90 grados y camina hacia la nueva dirección.\n";
+	cout << "7.-Continua hasta quedar de frente a una puerta, gira la perilla para liberar el seguro y empuja ligeramente para abirla\n";
+	cout << "8.-Ingresa a la nueva seccion, sujeta la perilla del otro lado de la puerta y empuja ligeramente para cerrarla\n";
+	cout << "9.-Has llegado al bano\n";
+	keep_window_open();
+	return 0;
+}
