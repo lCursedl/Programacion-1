010a769820e5ce9e007bb418de86e297c4dabd88
llCursedll <idv18c.galvarez@uartesdigitales.edu.mx>
2019-02-05 11:23:49 -0600

    Agregados ejercicios capitulos 4 y 5

--- Capitulo4/Capitulo4/Source.cpp
+++ Capitulo4/Capitulo4/Source.cpp
@@ -1,5 +1,17 @@
 #include "Includes.h"
 
+bool CheckPrime(int num, vector<int> VecNum)
+{
+	for (int i = 0; i < VecNum.size(); i++)
+	{
+		if (num % VecNum[i] == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 void Ejercicio3()
 {
 	vector <double> Distances;
@@ -162,9 +174,373 @@ void Ejercicio6()
 	cin.ignore();
 }
 
+void Ejercicio8()
+{
+	unsigned int CurrRice = 1;
+	unsigned int Square = 0;
+	for (CurrRice = 1; CurrRice < 1000; CurrRice *= 2)
+	{
+		Square += 1;
+	}
+	cout << "Se requieren " <<Square << " cuadros para obtener minimo de 1000 granos de arroz. Total: " << CurrRice << ".\n";
+	Square = 0;
+	for (CurrRice = 1; CurrRice < 1000000; CurrRice *= 2)
+	{
+		Square += 1;
+	}
+	cout << "Se requieren " << Square << " cuadros para obtener minimo de 1,000,000 granos de arroz. Total: " << CurrRice << ".\n";
+	Square = 0;
+	for (CurrRice = 1; CurrRice < 1000000000; CurrRice *= 2)
+	{
+		Square += 1;
+	}
+	cout << "Se requieren " << Square << " cuadros para obtener minimo de 1,000,000,000 granos de arroz. Total: " << CurrRice << ".\n";
+	cin.ignore();
+}
+
+void Ejercicio9()
+{
+	int iRice = 1;
+	double dRice = 1.;
+	for (int i = 1; i < 64; i++)
+	{
+		iRice *= 2;
+		dRice *= (double)2.;
+	}
+	cout << "Cantidad de granos en variable int: " << iRice << ".\n";
+	cout << "Cantidad de granos en variable double: " << dRice << ".\n";
+	cout << "Cantidad total verdadera: 18,446,744,073,709,551,616.\n";
+	cin.ignore();
+}
+
+void Ejercicio10()
+{
+	vector <int> PCChoice;
+	PCChoice.push_back(2);
+	PCChoice.push_back(3);
+	PCChoice.push_back(1);
+	int Player = 0;
+	cout << "Juego a 3 rondas.\n";
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\nIntroduce la jugada que quieras hacer:\n\tPiedra: 1. Papel: 2. Tijeras:3.\n";
+		cin >> Player;
+		switch (Player)
+		{
+		case 1:
+			if (PCChoice[i] == 1)
+			{
+				cout << "\nJugador: Piedra.\nPC: Piedra.\nEmpate.\n";
+			}
+			else if (PCChoice[i] == 2)
+			{
+				cout << "\nJugador: Piedra.\nComputadora: Papel.\nComputadora gana.\n";
+			}
+			else if (PCChoice[i] == 3)
+			{
+				cout << "\nJugador: Piedra.\nComputadora: Tijera.\nJugador gana.\n";
+			}
+			break;
+		case 2:
+			if (PCChoice[i] == 1)
+			{
+				cout << "\nJugador: Papel.\nPC: Piedra.\nJugador gana.\n";
+			}
+			else if (PCChoice[i] == 2)
+			{
+				cout << "\nJugador: Papel.\nComputadora: Papel.\Empate.\n";
+			}
+			else if (PCChoice[i] == 3)
+			{
+				cout << "\nJugador: Papel.\nComputadora: Tijera.\nComputadora gana.\n";
+			}
+			break;
+		case 3:
+			if (PCChoice[i] == 1)
+			{
+				cout << "\nJugador: Tijera.\nPC: Piedra.\nComputadora gana.\n";
+			}
+			else if (PCChoice[i] == 2)
+			{
+				cout << "\nJugador: Tijera.\nComputadora: Papel.\nJugador gana.\n";
+			}
+			else if (PCChoice[i] == 3)
+			{
+				cout << "\nJugador: Tijera.\nComputadora: Tijera.\nEmpate.\n";
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	cin.ignore();
+}
+
+void Ejercicio11()
+{
+	vector<int>PrimeNums;
+	for (int i = 2; i <= 100; i++)
+	{
+		if (CheckPrime(i, PrimeNums))
+		{
+			PrimeNums.push_back(i);
+		}
+	}
+	cout << "Numeros primos del 2 al 100:\n\n";
+	for (int i = 0; i < PrimeNums.size(); i++)
+	{
+		cout << PrimeNums[i] << "\n";
+	}
+	cin.ignore();
+}
+
+void Ejercicio12()
+{
+	vector<int>PrimeNums;
+	int PrimMax;
+	cout << "Introduzca el numero maximo postivo a calcular de numeros primos: ";
+	cin >> PrimMax;
+	for (int i = 2; i <= PrimMax; i++)
+	{
+		if (CheckPrime(i, PrimeNums))
+		{
+			PrimeNums.push_back(i);
+		}
+	}
+	cout << "Numeros primos del 2 al "<< PrimMax <<":\n\n";
+	for (int i = 0; i < PrimeNums.size(); i++)
+	{
+		cout << PrimeNums[i] << "\n";
+	}
+	cin.ignore();
+}
+
+void Ejercicio15()
+{
+	vector<int>PrimeNums;
+	int PrimeAmount;
+	int CurrAmount = 0;
+	int PrimeNum = 2;
+	cout << "Introduzca cantidad de numeros primos a calcular\n";
+	cin >> PrimeAmount;
+	while (CurrAmount != PrimeAmount)
+	{
+		if (CheckPrime(PrimeNum, PrimeNums))
+		{
+			PrimeNums.push_back(PrimeNum);
+			CurrAmount++;
+		}
+		PrimeNum++;
+	}
+	for (int i = 0; i < PrimeNums.size(); i++)
+	{
+		cout << PrimeNums[i] << "\n";
+	}
+	cin.ignore();
+}
+
+void Ejercicio18()
+{
+	double a;
+	double b;
+	double c;
+	double x1;
+	double x2;
+	cout << "Resolucion de ecuaciones de segundo grado (ax^2 + bx + c = 0)\n";
+	cout << "Introuzca el valor de a: ";
+	cin >> a;
+	cout << "Introduzca el valor de b: ";
+	cin >> b;
+	cout << "Introduzca el valor de c: ";
+	cin >> c;
+	x1 = pow(b, 2);
+	x2 = pow(b, 2);
+	x1 = x1 - (4 * a * c);
+	x2 = x2 - (4 * a * c);
+	x1 = sqrt(x1);
+	x2 = sqrt(x2);
+	x1 = -b + x1;
+	x2 = -b - x2;
+	x1 /= (2 * a);
+	x2 /= (2 * a);
+	cout << x1 << " " << x2;
+	cin.ignore();
+}
+
+void Ejercicio19()
+{
+	vector<string>Names;
+	vector<int>Scores;
+	string TempName;
+	int TempScore;
+	
+	bool RunProgram = true;
+	do
+	{
+		cout << "Introduzca un nombre: ";
+		cin >> TempName;
+		cout << "Introduzca un puntaje: ";
+		cin >> TempScore;
+		if (TempName != "NoName" && TempScore != 0)
+		{
+			for (int i = 0; i < Names.size(); i++)
+			{
+				if (TempName == Names[i])
+				{
+					cout << "Error. Nombre repetido\n";
+					cin.ignore();
+					cin.get();
+					return;
+				}
+			}
+			Names.push_back(TempName);
+			Scores.push_back(TempScore);
+		}
+		else
+		{
+			RunProgram = false;
+		}
+	} while (RunProgram);
+	for (int i = 0; i < Names.size(); i++)
+	{
+		cout << Names[i] << ", " << Scores[i] << "\n";
+	}
+	cin.ignore();
+}
+
+void Ejercicio20()
+{
+	vector<string>Names;
+	vector<int>Scores;
+	string TempName;
+	int TempScore;
+
+	bool RunProgram = true;
+	bool SearchName = false;
+	bool FoundName = false;
+
+	do
+	{
+		cout << "Introduzca un nombre: ";
+		cin >> TempName;
+		cout << "Introduzca un puntaje: ";
+		cin >> TempScore;
+		if (TempName != "NoName" && TempScore != 0)
+		{
+			for (int i = 0; i < Names.size(); i++)
+			{
+				if (TempName == Names[i])
+				{
+					cout << "Error. Nombre repetido\n";
+					cin.ignore();
+					cin.get();
+					return;
+				}
+			}
+			Names.push_back(TempName);
+			Scores.push_back(TempScore);
+		}
+		else
+		{
+			RunProgram = false;
+			SearchName = true;
+		}
+	} while (RunProgram);
+	
+	while (SearchName)
+	{
+		cout << "Introduzca un nombre para buscar su puntaje: ";
+		cin >> TempName;
+		for (int i = 0; i < Names.size(); i++)
+		{
+			if (Names[i] == TempName)
+			{
+				cout << Names[i] << ", " << Scores[i] << "\n";
+				FoundName = true;
+			}
+		}
+		if (!FoundName)
+		{
+			cout << "Nombre no encontrado\n";
+		}
+		else
+		{
+			FoundName = false;
+		}
+		cout << "Desea seguir buscando nombres?\n\tNo:0 Si:1\n\tSeleccion: ";
+		cin >> SearchName;
+	}
+	cin.ignore();
+}
+
+void Ejercicio21()
+{
+	vector<string>Names;
+	vector<int>Scores;
+	string TempName;
+	int TempScore;
+
+	bool RunProgram = true;
+	bool SearchScore = false;
+	bool FoundScore = false;
+
+	do
+	{
+		cout << "Introduzca un nombre: ";
+		cin >> TempName;
+		cout << "Introduzca un puntaje: ";
+		cin >> TempScore;
+		if (TempName != "NoName" && TempScore != 0)
+		{
+			for (int i = 0; i < Names.size(); i++)
+			{
+				if (TempName == Names[i])
+				{
+					cout << "Error. Nombre repetido\n";
+					cin.ignore();
+					cin.get();
+					return;
+				}
+			}
+			Names.push_back(TempName);
+			Scores.push_back(TempScore);
+		}
+		else
+		{
+			RunProgram = false;
+			SearchScore = true;
+		}
+	} while (RunProgram);
+
+	while (SearchScore)
+	{
+		cout << "Introduzca un puntaje para buscar todos los nombres correspondientes: ";
+		cin >> TempScore;
+		for (int i = 0; i < Scores.size(); i++)
+		{
+			if (Scores[i] == TempScore)
+			{
+				cout << Names[i] << "\n";
+				FoundScore = true;
+			}
+		}
+		if (!FoundScore)
+		{
+			cout << "Puntaje no encontrado\n";
+		}
+		else
+		{
+			FoundScore = false;
+		}
+		cout << "Desea seguir buscando puntajes?\n\tNo:0 Si:1\n\tSeleccion: ";
+		cin >> SearchScore;
+	}
+	cin.ignore();
+}
+
 int main()
 {
-	Ejercicio6();
+	Ejercicio20();
 	cin.get();
 	return 0;
 }
